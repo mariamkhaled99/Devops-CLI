f915668244bc11a38cb111220c84f25cb00999c1
fouad mahmoud <fouad_31057119@f-eng.tanta.edu.eg>
2025-05-24 16:39:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="syntera-ai",
-    version="0.1.0",
+    version="0.1.1",
     author="Fouad Mahmoud",
     author_email="fouadmahmoud281@gmail.com",
     description="An AI-powered DevOps toolkit for infrastructure automation and analysis",
