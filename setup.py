7a89c1284f41994e2db7451d7331b61791ca7d4e
mariam eissa <mariamkhaled999@gmail.com>
2025-08-03 12:14:18 +0300

    add new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="syntera-ai-cli",
-    version="0.1.4",
+    version="0.1.5",
     author="Fouad Mahmoud",
     author_email="fouadmahmoud281@gmail.com",
     description="An AI-powered DevOps toolkit for infrastructure automation and analysis",
